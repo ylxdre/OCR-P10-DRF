f4fc74dc7671114822013e8b1193758a3913f7f3
yann <yann@needsome.coffee>
2025-06-12 12:32:59 +0200

    fixed for flake

--- softdesk/support/admin.py
+++ softdesk/support/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from support.models import Project, Issue, Comment, ProjectContributor
 from authentication.models import User
 
+
 class AdminProject(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'contributors')
 
     @admin.display(description='contributors')
     def contributors(self, obj):
         return obj.contributors
+
 
 class AdminIssue(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'project')
@@ -18,4 +20,3 @@ admin.site.register(Project, AdminProject)
 admin.site.register(Issue, AdminIssue)
 admin.site.register(Comment)
 admin.site.register(ProjectContributor)
-
